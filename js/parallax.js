d3d44e1052b41e9fe8a0ee44a031637f47bcc4ad
Brooke Rhodes <6473667+grilme99@users.noreply.github.com>
2021-02-10 13:18:33 +0000

    Initial commit

---
+++ js/parallax.js
@@ -0,0 +1,19 @@
+function createParallax() {
+	const layers = document.getElementsByClassName('parallax')
+
+	window.addEventListener('scroll', function (event) {
+		const top = this.pageYOffset
+		for (const layer of layers) {
+			const speed = layer.getAttribute('data-speed')
+			const yPos = -((top * speed) / 100)
+			layer.setAttribute(
+				'style',
+				`transform: translate3d(0px, ${yPos}px, 0px)`
+			)
+		}
+	})
+}
+
+document.body.onload = function () {
+	createParallax()
+}
